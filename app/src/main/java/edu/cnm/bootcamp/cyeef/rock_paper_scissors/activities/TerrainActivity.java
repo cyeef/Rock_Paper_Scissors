9853c9abba4ae268a103114e14cdfeb2344da7a5
cyeef <cyeef@icloud.com>
2017-10-18 16:29:01 -0600

    Initial commit Rock Paper Scissors

---
+++ app/src/main/java/edu/cnm/bootcamp/cyeef/rock_paper_scissors/activities/TerrainActivity.java
@@ -0,0 +1,14 @@
+package edu.cnm.bootcamp.cyeef.rock_paper_scissors.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import edu.cnm.bootcamp.cyeef.rock_paper_scissors.R;
+
+public class TerrainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_terrain);
+  }
+}
